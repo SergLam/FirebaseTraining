592d8fe027dfd243a81e3e2cf6c21cfc4947d396
SergLam <lamcevs0@gmail.com>
2018-07-23 12:03:36 +0300

    base logic of viewcontroller switching

--- FirebaseAuthTraining/UserStory/Entrance/EntranceVC.swift
+++ FirebaseAuthTraining/UserStory/Entrance/EntranceVC.swift
@@ -12,11 +12,10 @@ import Eureka
 
 class EntranceVC: FormViewController {
     
-    let signInVC = SignInVC()
-    
-    let signUpVC = SignUpVC()
-    
+    var childControllers: [UIViewController] = []
     let containerView = UIView()
+    let pages = ["Sign In", "Sign Up"]
+    let segment = UISegmentedControl()
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -29,7 +28,19 @@ class EntranceVC: FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let signInVC = SignInVC()
+        let signUpVC = SignUpVC()
+        
+        childControllers.append(contentsOf: [signInVC, signUpVC])
+        
+        childViewControllers.forEach {
+            addChildViewController($0)
+            $0.didMove(toParentViewController: self)
+        }
+        
         configureUI()
+        
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -42,40 +53,55 @@ class EntranceVC: FormViewController {
     }
     
     func configureUI(){
-        view.backgroundColor = .gray
-//        let pages = ["Sign In", "Sign Up"]
-//        let segment = UISegmentedControl.init(items: pages)
-//        segment.layer.cornerRadius = 5.0
-//        segment.backgroundColor = UIColor.white
-//        segment.selectedSegmentIndex = 0
-//        segment.addTarget(self, action: #selector(self.changeVisibleVC(sender:)), for: UIControlEvents.valueChanged)
-//
-//        self.view.addSubview(segment)
-//        segment.snp.makeConstraints{ (make) -> Void in
-//            make.height.equalTo(30)
-//        make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin)
-//            make.left.equalTo(self.view).offset(10)
-//            make.right.equalTo(self.view).offset(-10)
-//        }
-        form +++ Section("Section1")
-            <<< TextRow(){ row in
-                row.title = "Text Row"
-                row.placeholder = "Enter text here"
-            }
-            <<< PhoneRow(){
-                $0.title = "Phone Row"
-                $0.placeholder = "And numbers here"
-            }
-            +++ Section("Section2")
-            <<< DateRow(){
-                $0.title = "Date Row"
-                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+        for (index, page) in pages.enumerated() {
+          segment.insertSegment(withTitle: page, at: index, animated: false)
+        }
+        segment.layer.cornerRadius = 5.0
+        segment.backgroundColor = UIColor.white
+        segment.selectedSegmentIndex = 0
+        segment.addTarget(self, action: #selector(self.changeVisibleVC(sender:)), for: UIControlEvents.valueChanged)
+
+        self.view.addSubview(segment)
+        segment.snp.makeConstraints{ (make) -> Void in
+            make.height.equalTo(30)
+        make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin)
+            make.left.equalTo(self.view).offset(10)
+            make.right.equalTo(self.view).offset(-10)
         }
         
+        containerView.backgroundColor = .green
+        self.view.addSubview(containerView)
+        containerView.snp.makeConstraints{ make -> Void in
+            make.top.equalTo(segment.snp.bottom).offset(10)
+            make.left.equalTo(self.view)
+            make.right.equalTo(self.view)
+            make.bottom.equalTo(self.view)
+        }
+        
+        setViewControllerToView(vc: childControllers[1])
+        setViewControllerToView(vc: childControllers[0])
+
+
     }
     
    @objc func changeVisibleVC(sender: UISegmentedControl){
-        print("Click")
+    print("Clicked \(sender.titleForSegment(at: sender.selectedSegmentIndex)!)")
+    switch sender.selectedSegmentIndex {
+        // Such approach didn't give memory leaks
+    case 0:
+        childControllers[0].view.alpha = 1
+        childControllers[1].view.alpha = 0
+    case 1:
+        childControllers[0].view.alpha = 0
+        childControllers[1].view.alpha = 1
+    default:
+        break
+    }
+    }
+    
+    func setViewControllerToView(vc: UIViewController){
+        vc.view.frame = self.containerView.bounds
+        self.containerView.addSubview(vc.view)
     }
     
 }
