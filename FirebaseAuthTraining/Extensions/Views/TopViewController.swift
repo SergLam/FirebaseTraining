de971a4b12052798b9bd3b18ce3e566324df7fcc
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2018-11-21 14:02:03 +0200

    Added .gitignore file

---
+++ FirebaseAuthTraining/Extensions/Views/TopViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TopViewController.swift
+//  FirebaseAuthTraining
+//
+//  Created by Serg Liamthev on 7/25/18.
+//  Copyright © 2018 SergLam. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+    class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
+}
+
+extension UIViewController{
+    
+    func hideNavBar(_ isHidden: Bool){
+        self.navigationController?.setNavigationBarHidden(isHidden, animated: true)
+    }
+}
